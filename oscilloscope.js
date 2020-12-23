05a2fe8f8a289de6631bd4d058dd3c5caf4a2c0f
Oscar Saharoy <osaharoy@gmail.com>
2020-12-23 14:58:30 +0000

    scrolling smoother now

--- oscilloscope.js
+++ oscilloscope.js
@@ -12,13 +12,12 @@ button.onclick = async event => {
 
     button.onclick = null;
     button.style.display = "none";
-    n = 0;
 
     port = await navigator.serial.requestPort();
 
     console.log("got port!")
 
-    await port.open({ baudRate: 9600 });
+    await port.open({ baudRate: 115200 });
 
     console.log("opened port!");
 
@@ -28,22 +27,42 @@ button.onclick = async event => {
 
     console.log("got reader!");
 
+    await collectData(reader);
+};
+
+async function collectData(reader) {
+    
+    var n = 0;
     var halfstring = null;
 
-    // Listen to data coming from the serial device.
+    // listen to data coming from the serial device
     while (true) {
 
         const { value, done } = await reader.read();
-        var strings = value.split("\n")
+        var strings = value.split("\n");
 
         if(halfstring) strings = [halfstring + strings[0], ...strings];
 
         halfstring = !isWellFormed( strings[strings.length-1] ) ? strings.pop() : null;
 
         const newPoints = strings.filter( isWellFormed ).map( parseFloat ).map( x => new vec2(n++ / SAMPLERATE, x) );
-        graphjs.originOffset.x -= newPoints.length / SAMPLERATE;
         graphjs.addPoints( newPoints );
 
         if (n > SAMPLERATE * 15) graphjs.points.splice(0, newPoints.length); // only keep 15 seconds of history
     }
-};
+}
+
+var currentTime = null;
+
+async function scroll( time ) {
+    
+    if(currentTime)
+        graphjs.originOffset.x -= ( time - currentTime ) / 1000;
+    
+    currentTime = time;
+    graphjs.redraw();
+
+    requestAnimationFrame( scroll );
+}
+
+requestAnimationFrame( scroll );
